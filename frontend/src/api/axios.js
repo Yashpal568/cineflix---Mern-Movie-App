9fb37b3d99f9985023c3880b9cea7d97711753fa
Yashpal568 <yash38687@gmail.com>
2025-11-21 18:23:00 +0530

    fix axios url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://cineflix-mern-movie-app.onrender.com/api/v1",
 });
 
 // It Attach token automatically
